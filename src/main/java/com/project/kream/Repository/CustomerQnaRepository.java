4dde81f9cecfa409a2dd6193194c36aef35ab8b6
soyounjeong <soyounjeong6276@gmail.com>
2022-01-04 00:48:20 +0900

    22-01-03

--- src/main/java/com/project/kream/Repository/CustomerQnaRepository.java
+++ src/main/java/com/project/kream/Repository/CustomerQnaRepository.java
@@ -3,5 +3,5 @@ package com.project.kream.Repository;
 import com.project.kream.Model.Entity.CustomerQna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CunstomerQnaRepository extends JpaRepository<CustomerQna, Long> {
+public interface CustomerQnaRepository extends JpaRepository<CustomerQna, Long> {
 }
